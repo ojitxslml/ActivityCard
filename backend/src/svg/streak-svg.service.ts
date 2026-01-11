10d15d7e7042e5547fa640ca5b9c6960f0bec26f
Juan Pablo Urra <jp.urra42@gmail.com>
2026-01-11 15:09:19 -0300

    reduce opacity of streak fire

--- backend/src/svg/streak-svg.service.ts
+++ backend/src/svg/streak-svg.service.ts
@@ -124,10 +124,10 @@ export class StreakSvgService {
       <path 
         d="M8.5 14.5A2.5 2.5 0 0 0 11 12c0-1.38-.5-2-1-3-1.072-2.143-.224-4.054 2-6 .5 2.5 2 4.9 4 6.5 2 1.6 3 3.5 3 5.5a7 7 0 1 1-14 0c0-1.153.433-2.294 1-3a2.5 2.5 0 0 0 2.5 2.5z" 
         fill="#${color}" 
-        fill-opacity="0.2"
+        fill-opacity="0.1"
         stroke="#${color}"
         stroke-width="0.4" 
-        stroke-opacity="0.9"
+        stroke-opacity="0.4"
         stroke-linejoin="round"
         stroke-linecap="round"
       />
